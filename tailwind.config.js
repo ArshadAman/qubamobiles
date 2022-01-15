a98c595ca7516fbe1dd4b4fe328bb5acd00f8966
codeinput7 <arshadaman202@gmail.com>
2022-01-15 23:14:09 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'mera-green':'#24A19C',
+        'ghee':'#FAEEE7',
+        'mera-blue':'#325288',
+      }
+    },
+  },
+  plugins: [],
+}
